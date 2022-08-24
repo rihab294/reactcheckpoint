21dc09168af2f84955fc0f3f62e62a4e82ed7977
rihab294 <rihabbet@gmail.com>
2022-08-24 14:54:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './Components/Navbar'
+import Formm from './Components/Formm'
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Formm />
+    </div>
+  );
+}
+
+export default App;
